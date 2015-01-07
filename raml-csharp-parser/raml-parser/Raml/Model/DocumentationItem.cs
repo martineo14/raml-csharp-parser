d9b47fcbc60c7edaafd6c7e6c86ef645ef159846
Martin Pueyo <martinpueyo@gmail.com>
2015-01-07 00:57:31 -0300

    MIgrated Model  DocumentationItem and the Enum Protocol.

--- raml-csharp-parser/raml-parser/Raml/Model/DocumentationItem.cs
+++ raml-csharp-parser/raml-parser/Raml/Model/DocumentationItem.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿/*
+ * Copyright 2015 (c) Sergio Martin Pueyo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Raml.Parser.Raml.Model
 {
-    class DocumentationItem
+    [Serializable]
+    public class DocumentationItem
     {
+    private static readonly long serialVersionUID = 8041314633068025129L;
+
+    [Required()]
+    public string Title{get;set;}
+
+    [Required()]
+    public string Content{get;set;}
+
     }
 }
